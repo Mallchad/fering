fda21493b0a3fba13d97de2f5a93e6992eb23772
Mallchad <ncaprisunfan@gmail.com>
2021-01-18 14:34:17 +0000

    Addition: Set red color for no recorded entries output

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,30 @@
 #include <exception>
 #include <fstream>
 
+#ifdef linux
+#include <cstdlib>
+// Constants
+//the following are UBUNTU/LINUX, and MacOS ONLY terminal color codes.
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+#endif
 using namespace std::literals::string_literals;
+
 static bool verbose_output = false;
 /**
  * @brief Iterates over file and directory paths and returns a list of files that exists.
@@ -83,7 +106,7 @@ bool database_add_records(std::fstream& open_database,
     }
     else
     {
-        std::cout << "No entries were recorded \n";
+        std::cout << RED "No entries were recorded \n" WHITE;
     }
     return true;
 }
